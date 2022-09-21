8d0816ffa80b92ee9592d609d9265aa3b7f90f00
IM <ma.ishikawa.sys22@morijyobi.ac.jp>
2022-09-21 12:19:04 +0900

    pra2.javaに出力処理追記

--- pra2.java
+++ pra2.java
@@ -1,7 +1,5 @@
-class Helloworld{
-      public static void main(String[] args){
-	     for(int i = 0; i < 100; i++){ 
-               System.out.println("Hello World");
-	    }
-      }
+class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
 }
